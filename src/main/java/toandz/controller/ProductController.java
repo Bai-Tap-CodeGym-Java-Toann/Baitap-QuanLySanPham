6776059997a616aeb1fb39f847edfb54e9b20e14
Nguyễn Đại Toàn <sitdownrightnow2552@gmail.com>
2018-10-23 13:57:00 +0700

    all done.

--- src/main/java/toandz/controller/ProductController.java
+++ src/main/java/toandz/controller/ProductController.java
@@ -2,20 +2,19 @@ package toandz.controller;
 
 import toandz.model.Product;
 import toandz.service.ProductsServ;
-import toandz.service.ProductsService;
+
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.HttpMethodConstraint;
-import javax.servlet.annotation.ServletSecurity;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+
 import java.util.ArrayList;
-import java.util.HashMap;
+
 
 @WebServlet(name = "ProductController", urlPatterns = "/products")
 public class ProductController extends HttpServlet {
